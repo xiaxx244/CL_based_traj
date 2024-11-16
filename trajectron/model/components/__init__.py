99d5c1f5cb9c59ec195d6774ba44e97399c37132
Youya Xia <32881987+xiaxx244@users.noreply.github.com>
2024-11-16 16:57:59 -0500

    Update __init__.py for gmm2d1

--- trajectron/model/components/__init__.py
+++ trajectron/model/components/__init__.py
@@ -1,4 +1,5 @@
 from .discrete_latent import DiscreteLatent
+from .gmm2d1 import GMM2D1
 from .gmm2d import GMM2D
 from .map_encoder import CNNMapEncoder
 from .additive_attention import AdditiveAttention, TemporallyBatchedAdditiveAttention
